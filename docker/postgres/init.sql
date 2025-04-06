66f99f36be7e572847357a10e7f9714f353d033c
Eugene Vanchugov <EVanchugov@alfabank.ru>
2025-04-06 23:26:29 +0800

    fix: remove unnecessary schema creation in PostgreSQL init script

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -3,8 +3,5 @@
 -- Включение расширения pgvector для векторной БД
 CREATE EXTENSION IF NOT EXISTS vector;
 
--- Создание рабочей схемы
-CREATE SCHEMA IF NOT EXISTS lightrag;
-
 -- Проверка установки расширений
 SELECT extname, extversion FROM pg_extension WHERE extname = 'vector';
